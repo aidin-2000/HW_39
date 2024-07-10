0b5301f6dfc15b3d390a64661d9d840da11cb399
Айдин Нурланбеков <nurlanbekovich.a@gmail.com>
2024-07-10 20:18:06 +0600

    Добавьте файлы проекта.

---
+++ HW_39/Controllers/HW39Controller.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HW_39.Controllers
+{
+    [ApiController]
+    [Route("controller")]
+    public class HW39Controller //: Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Дима Гей";
+        }
+        
+      
+    }
+}
